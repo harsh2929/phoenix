597f8bfd1494fb9871dde9f989f55e47272ede30
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-09 18:10:16 +0530

    Adding label to axes

--- packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/view-options/view-options.component.ts
+++ packages/phoenix-ng/projects/phoenix-ui-components/lib/components/ui-menu/view-options/view-options.component.ts
@@ -32,3 +32,33 @@ export class ViewOptionsComponent implements OnInit {
     this.eventDisplay.getUIManager().setShowGrid(value);
   }
 }
+const fontLoader = new THREE.FontLoader();
+
+// Load the font file (this may take some time)
+fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+
+  // Create a new TextGeometry for the x-axis label
+  const xLabelGeometry = new THREE.TextGeometry('x', {
+    font: font,
+    size: 50,
+    height: 10,
+    curveSegments: 12
+  });
+
+ 
+  const xLabelMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+  const xLabelMesh = new THREE.Mesh(xLabelGeometry, xLabelMaterial);
+  xLabelMesh.position.set(500, 0, 0);
+    const yLabelMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+  const yLabelMesh = new THREE.Mesh(yLabelGeometry, yLabelMaterial);
+  yLabelMesh.position.set(500, 0, 0);
+  
+    const zLabelMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+  const zLabelMesh = new THREE.Mesh(zLabelGeometry, zLabelMaterial);
+  xLabelMesh.position.set(500, 0, 0);
+  this.scene.add(xLabelMesh);
+  this.scene.add(yLabelMesh);
+  this.scene.add(zLabelMesh);
+  
+
+});
